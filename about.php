b537662b0cdd9309c0ae951ff32e73c61ebfe5fe
nathantran00 <nathantrxn@gmail.com>
2023-12-10 03:26:27 -0800

    Added about page and lineup lightbox with JSON data

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>queue music festival</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/about.css">
+</head>
+
+
+<body>
+    <?php require("partials/header.php"); ?>
+
+    <div class="container">
+        
+        <section class="responsive">
+            <img src="img/about-banner-1.jpg" alt="Alt text for banner 1" aria-label="Aria label for banner 1">
+            <img src="img/about-banner-2.jpg" alt="Alt text for banner 2" aria-label="Aria label for banner 2">
+            <img src="img/about-banner-3.jpg" alt="Alt text for banner 3" aria-label="Aria label for banner 3">
+            <img src="img/about-banner-4.jpg" alt="Alt text for banner 4" aria-label="Aria label for banner 4">
+        </section>
+
+    </div>
+
+    <?php require("partials/footer.php"); ?>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+        integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="js/slick.js"></script>
+</body>
